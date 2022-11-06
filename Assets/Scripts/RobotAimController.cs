dd4f45075af4eb5d6a991ea859f15865cf4d61b8
lutca1320@gmail.com <>
2022-11-06 14:14:42 +0900

    Fix Aim GameObject

--- Assets/Scripts/RobotAimController.cs
+++ Assets/Scripts/RobotAimController.cs
@@ -24,24 +24,37 @@ public class RobotAimController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        int groundLayer = 1 << LayerMask.NameToLayer("Ground");
+        int obstacleLayer = 1 << LayerMask.NameToLayer("Obstacle");
+
         Vector3 targetPos = transform.position + (enemyRobotAI ? enemyRobotAI.navAgent.velocity : transform.forward * 3);
+        targetPos.y = transform.root.position.y + (inputHandler.isCrouch ? 0.7f : 1.25f);
 
         if (cam)
         {
             Ray ray = cam.mainCam.ScreenPointToRay(Input.mousePosition);
-            int layerMask = (1 << LayerMask.NameToLayer("Ground")) | (1 << LayerMask.NameToLayer("Obstacle"));
-            Physics.Raycast(ray, out RaycastHit hit, 1000, layerMask);
-            targetPos = hit.collider ? hit.point : transform.root.position + transform.forward * 3;
+            Physics.Raycast(ray, out RaycastHit groundHit, 1000, groundLayer);
+            Physics.Raycast(ray, out RaycastHit obstacleHit, 1000, obstacleLayer);
+
+            if (obstacleHit.collider)
+            {
+                targetPos = obstacleHit.point;
+            }
+            else if (groundHit.collider)
+            {
+                targetPos = groundHit.point;
+                targetPos.y = transform.root.position.y + (inputHandler.isCrouch ? 0.7f : 1.25f);
+            }
+            else
+            {
+                targetPos = transform.root.position + transform.root.forward * 3;
+                targetPos.y = transform.root.position.y + (inputHandler.isCrouch ? 0.7f : 1.25f);
+            }
         }
         else if (enemyRobotAI.enemyObject)
         {
             targetPos = enemyRobotAI.enemyObject.transform.position;
         }
-
-        Vector3 direction = (targetPos - transform.root.position).normalized;
-        targetPos = direction * 3 + transform.root.position;
-
-        targetPos.y = inputHandler.isCrouch ? 0.7f : 1.25f;
 
         gameObject.transform.position = targetPos;
 
